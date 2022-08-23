20b297f2c2046bb4435c3a305c4e6849471ef10c
Micah Bessolo <83624003+micahbessolo@users.noreply.github.com>
2022-08-23 06:10:55 -0700

    new server ip address

--- api/app.js
+++ api/app.js
@@ -4,14 +4,14 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 // To make this run on secure server
-const fs = require('fs');
-const https = require('https');
-const path = require('path');
+// const fs = require('fs');
+// const https = require('https');
+// const path = require('path');
 
-const sslServer = https.createServer({
-  key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
-  cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
-}, app)
+// const sslServer = https.createServer({
+//   key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+//   cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
+// }, app)
 
 const {mongoose} = require('./db/mongoose');
 
@@ -26,6 +26,10 @@ app.use(bodyParser.json());
 // Cors Header Middleware
 app.use(cors());
 
+// app.use('/', (req, res, next) => {
+//   res.send('hello from ssl server')
+// })
+
 app.use(function(req, res, next) {
   // updates to match the domain you make the request from
   res.header("Access-Control-Allow-Origin", "*");
@@ -35,7 +39,6 @@ app.use(function(req, res, next) {
 
 // Loads in the mongoose models/schemas
 const { Feed } = require('./db/models/feed.model');
-const { createServer } = require('http');
 
 // GET /feeds
 app.get('/feeds', (req, res) => {
@@ -72,8 +75,9 @@ app.delete('/feeds/:id', (req, res) => {
   })
 });
 
-sslServer.listen(3010, () => console.log("secure server"));
 
-// app.listen(port, () => {
-//   console.log("Server is listening on port " + port);
-// })
+// sslServer.listen(3010, () => console.log("secure server"));
+
+app.listen(port, () => {
+  console.log("Server is listening on port " + port);
+})
