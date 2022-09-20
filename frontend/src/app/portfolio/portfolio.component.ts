b11a1ce2086f191a5ef7c50fa53e281174c32d5a
Micah Bessolo <83624003+micahbessolo@users.noreply.github.com>
2022-09-19 17:40:14 -0700

    portfolio updated to cards rather than folder

--- frontend/src/app/portfolio/portfolio.component.ts
+++ frontend/src/app/portfolio/portfolio.component.ts
@@ -10,37 +10,5 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 export class PortfolioComponent {
 
-clickedButton(num: number) {
-  const tabArray = [document.getElementById("website"), document.getElementById("profile"), document.getElementById("contact")];
-  const btnArray = [document.getElementById("website-tab"), document.getElementById("profile-tab"), document.getElementById("contact-tab")];
-
-  let clickedButton = btnArray[num];
-  let clickedTab = tabArray[num];
-
-  if (clickedButton?.classList.contains('active')) {}
-  else {
-    clickedButton?.classList.add("active");
-    clickedTab?.classList.add("active");
-  };
-
-  for (let i = 0; i < btnArray.length; i++){
-    if (i !== num && btnArray[i]?.classList.contains('active')){
-      btnArray[i]?.classList.remove("active");
-      tabArray[i]?.classList.remove("active");
-    }
-  }
-};
-
-accordionButton() {
-  const backBtn = document.getElementById("backend");
-  const frontBtn = document.getElementById("frontend");
-  const backendCollapse = document.getElementById("backendCollapse");
-  const frontCollapse = document.getElementById("frontendCollapse");
-
-  backBtn?.classList.toggle("collapsed");
-  frontBtn?.classList.toggle("collapsed");
-  backendCollapse?.classList.toggle("show");
-  frontCollapse?.classList.toggle("show");
-};
 
 }
