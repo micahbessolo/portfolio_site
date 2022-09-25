abaaea212a044e18aebe74a44621c9c73fed10c4
Micah Bessolo <83624003+micahbessolo@users.noreply.github.com>
2022-09-25 13:01:05 -0700

    fixing backend

--- api/db/mongoose.js
+++ api/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_ATLAS, { useNewUrlParser: true }).then(() => {
+mongoose.connect('mongodb+srv://micahbessolo:1Cdattcwsm.@contactfeed.s3sczr4.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true }).then(() => {
   console.log("connected to MongoDB");
 }).catch((e) => {
   console.log("Error while attempting to connect to MongoDB");
